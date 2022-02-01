085681e12a5d0c219113020296d0120830ac4b56
keikamiguchi <kei.kamiguchi517@gmail.com>
2022-02-01 14:14:58 +0900

    [update]プロジェクト名変更

--- model_form_app/asgi.py
+++ model_form_app/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for dproject project.
+ASGI config for model_form_app project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dproject.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'model_form_app.settings')
 
 application = get_asgi_application()
