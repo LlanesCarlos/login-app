58db014b55fe7ab1f3fe483fcfcfbcde64b5a21d
keikamiguchi <kei.kamiguchi517@gmail.com>
2022-03-10 14:23:52 +0900

    [update]アプリ名変更

--- adminSite/asgi.py
+++ adminSite/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for model_form_app project.
+ASGI config for adminSite project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'model_form_app.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'adminSite.settings')
 
 application = get_asgi_application()
--- blog/models.py
+++ blog/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class Article(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField()
+
+    def __str__(self):
+        return f'{self.title}, {self.content}'
