1591bef9ec7bf2ebc33155c49e40d30ed94d1ede
LlanesCarlos <cllanes.llanes@gmail.com>
2024-07-10 11:32:24 -0400

    Update settings.py

--- loginApp/settings.py
+++ loginApp/settings.py
@@ -110,9 +110,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'ja'
+LANGUAGE_CODE = 'es'
 
-TIME_ZONE = 'Asia/Tokyo'
+TIME_ZONE = 'America/Asuncion'
 
 USE_I18N = True
 
